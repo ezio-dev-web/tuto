48c66e1874c4c4a8169ec876330261f9164898cc
cris <cristian_manuel@outlook.com>
2024-01-08 13:40:40 -0500

    workflow finished

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'preact/hooks'
+
+export default function Greeting({messages}) {
+  const randomMessage = () => messages[(
+    Math.floor(Math.random() * messages.length)
+  )]
+  const [greeting, setGreeting] = useState(messages[0])
+
+  return (
+    <div>
+      <h3>{greeting}! Gracias por la Visita</h3>
+      <button onClick={() => setGreeting(randomMessage())}>
+        Nuevo Saludo
+      </button>
+    </div>
+  )
+}
